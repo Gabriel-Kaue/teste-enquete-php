d9054457d5aeb2f78d6c9cb0efd1e247a28c627e
Gabriel Alves <gabrielkauecaitan099@gmail.com>
2025-02-16 12:28:06 -0300

    feat: tela inicial

---
+++ enquete-php/resources/views/layouts/main.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono" rel="stylesheet">
+</head>
+<body>
+    @yield('content')
+    <footer>
+        <p> Enquete PHP ltda</p>
+    </footer>
+</body>
+</html>
