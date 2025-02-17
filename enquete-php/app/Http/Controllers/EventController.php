3f42768b381099dedef0d20ee82a9bc436aec962
Gabriel Alves <gabrielkauecaitan099@gmail.com>
2025-02-17 20:47:51 -0300

    feat events

--- enquete-php/app/Http/Controllers/EventController.php
+++ enquete-php/app/Http/Controllers/EventController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Event;
 
 class EventController extends Controller
 {
     public function index(){
+        $events = Event::all();
+        //return view("welcome",['events'=>$events]);
 
     }
     public function create(){
