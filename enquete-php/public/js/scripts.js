14ebd84d33a78556e56d6ecab287f7df8cc1c81a
Gabriel Alves <gabrielkauecaitan099@gmail.com>
2025-02-18 17:21:50 -0300

    fix: tabela dinâmica

--- enquete-php/public/js/scripts.js
+++ enquete-php/public/js/scripts.js
@@ -2,15 +2,30 @@ var formfield = document.getElementById("formfield");
 
 function add() {
     var novoCampo = document.createElement("input");
+    var ul = formfield.querySelector(".wrapper");
+
+    var newLi = document.createElement("li");
+    newLi.className = "form-row";
     novoCampo.setAttribute("type", "text");
     novoCampo.setAttribute("name", "resposta");
     novoCampo.setAttribute("placeholder", "resposta");
-    formfield.appendChild(novoCampo);
+
+    newLi.appendChild(novoCampo);
+    ul.appendChild(newLi);
 }
 
 function remover() {
-    var inputT = formfield.getElementsByTagName("input");
-    if (inputT.length > 6) {
-        formfield.removeChild(inputT[inputT.length - 1]);
+    var ul = formfield.querySelector(".wrapper");
+    var liElements = ul.getElementsByTagName("li");
+
+    if (liElements.length > 6) {
+        var lastLi = liElements[liElements.length - 1];
+        var inputElements = lastLi.getElementsByTagName("input");
+        if (inputElements.length > 0) {
+            lastLi.removeChild(inputElements[inputElements.length - 1]);
+            if (lastLi.children.length === 0) {
+                ul.removeChild(lastLi);
+            }
+        }
     }
 }
--- enquete-php/resources/views/events/create.blade.php
+++ enquete-php/resources/views/events/create.blade.php
@@ -6,14 +6,28 @@
     <h1>Crie sua enquete</h1>
     <form action="/events" method="post">
         <div id="formfield">
-            <input type="text" name="titulo" placeholder="Titulo" required>
-            <input type="datetime-local" name="dtInicio" placeholder="Data de Inicio" required>
-            <input type="datetime-local" name="dtFim" placeholder="Data de Fim" required>
-            <input type="text" name="resposta" class="respostas" placeholder="resposta" required>
-            <input type="text" name="resposta" class="respostas" placeholder="resposta" required>
-            <input type="text" name="resposta" class="respostas" placeholder="resposta" required>
+            <ul class="wrapper">
+                <li class="form-row">
+                    <input type="text" name="titulo" placeholder="Titulo" required>
+                </li>
+                <li class="form-row">
+                    <input type="datetime-local" name="dtInicio" placeholder="Data de Inicio" required>
+                </li>
+                <li class="form-row">
+                     <input type="datetime-local" name="dtFim" placeholder="Data de Fim" required>
+                </li>
+                <li class="form-row">
+                    <input type="text" name="resposta" class="respostas" placeholder="resposta" required>
+                </li>
+                <li class="form-row">
+                    <input type="text" name="resposta" class="respostas" placeholder="resposta" required>
+                </li>
+                <li class="form-row">
+                    <input type="text" name="resposta" class="respostas" placeholder="resposta" required>
+                </li>
+            </ul>
         </div>
-        <input type="submit" value="Submit">
+        <input type="submit" value="CRIAR ENQUETE">
     </form>
     <div>
         <button class="add" onclick="add()"><i class="mais"></i>Criar Resposta</button>
